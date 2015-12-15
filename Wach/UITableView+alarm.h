3a46837b99c77ca2b3a2523ef63908f61782ad05
AlexRusnac <rusnacalexandru@rusnacs-macbook-pro-2.local>
2015-12-15 18:02:13 +0200

    alarm

---
+++ Wach/UITableView+alarm.h
@@ -0,0 +1,13 @@
+//
+//  UITableView+alarm.h
+//  Wach
+//
+//  Created by Rusnac Alexandru on 12/15/15.
+//  Copyright © 2015 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableView (alarm)
+
+@end
---
+++ Wach/UITableView+alarm.m
@@ -0,0 +1,13 @@
+//
+//  UITableView+alarm.m
+//  Wach
+//
+//  Created by Rusnac Alexandru on 12/15/15.
+//  Copyright © 2015 User. All rights reserved.
+//
+
+#import "UITableView+alarm.h"
+
+@implementation UITableView (alarm)
+
+@end
