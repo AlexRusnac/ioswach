6811e1e80a3f295bdc7450f77d6c8ec7c3b2d7da
User <user@imac-4.local>
2015-12-17 17:25:20 +0200

    18.00

--- Wach/ViewController.h
+++ Wach/ViewController.h
@@ -11,6 +11,7 @@
 @interface ViewController : UIViewController
 
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *addtable;
+@property (weak, nonatomic) IBOutlet UINavigationItem *editraw;
 
 @end
 
--- Wach/ViewController.m
+++ Wach/ViewController.m
@@ -32,7 +32,7 @@
     
     // Do any additional setup after loading the view, typically from a nib.
     
-    
+      
 }
 
 
